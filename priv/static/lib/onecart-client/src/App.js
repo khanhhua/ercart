4c0d8c3c78faecf2435cd89c6d4cef58be9d3432
Hua Do Gia Khanh <khanh.hua@axa.com.sg>
2018-02-14 12:03:15 +0800

    Show-hide cart

--- priv/static/lib/onecart-client/src/App.js
+++ priv/static/lib/onecart-client/src/App.js
@@ -1,18 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
+// import logo from './logo.svg';
 import './App.css';
 
+const VIEW_NONE = 'none';
+const VIEW_SUMMARY = 'summary';
+const VIEW_CART = 'cart';
+
 class App extends Component {
+  state = {
+    view: VIEW_NONE,
+    items: [
+      {
+        product: 'F22 Raptor',
+        qty: 1
+      },
+      {
+        product: 'B52 Carpet Bomber',
+        qty: 2
+      }
+    ]
+  }
+
+  hideAll () {
+    this.setState({view: VIEW_NONE});
+  }
+
+  showSummary () {
+    this.setState({view: VIEW_SUMMARY});
+  }
+
+  showCart () {
+    this.setState({view: VIEW_CART}, () => {
+      const node = ReactDOM.findDOMNode(this.modal);
+      node.style = 'display:block;';
+
+      setTimeout(() => {
+        node.classList.add('show');
+      }, 300);
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="onecart">
+        {(this.state.view === VIEW_SUMMARY)&&
+        <div className="onecart-summary">
+          <h2>Summary</h2>
+          <p>Cart summary</p>
+          <div className="btn-group">
+            <a className="btn btn-default" href="#" onClick={() => this.hideAll()}>Close</a>
+            <a className="btn btn-primary" href="#" onClick={() => this.showCart()}>Details</a>
+          </div>
+        </div>
+        }
+        {(this.state.view === VIEW_CART)&&
+        <div className="onecart-cart">
+          <div className="modal fade" tabIndex="-1" role="dialog" ref={(node) => this.modal = node}>
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Your cart</h5>
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                <ul className="list-group list-group-flush">
+                {this.state.items.map((it, i) =>
+                  <li className="list-group-item" key={i}>
+                    {it.product}
+                    <div className="float-right input-group" style={{width: 140}}>
+                      <input type="number" className="form-control" value={it.qty} />
+                      <div className="input-group-append">
+                        <button className="btn btn-outline-secondary" type="button">Remove</button>
+                      </div>
+                    </div>
+                  </li>
+                )}
+                </ul>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-primary">Check out now!</button>
+                  <button type="button" className="btn btn-secondary" onClick={() => this.hideAll()}>Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        }
       </div>
     );
   }
--- priv/static/lib/onecart-client/src/index.js
+++ priv/static/lib/onecart-client/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-export default function (selector) {
-  ReactDOM.render(<App />, document.querySelector(selector));
-}
+// Global exports
+
+window.ONECART = {
+  init ({appid}) {
+    const root = document.createElement('div');
+    root.setAttribute('id','onecart-root');
+    document.body.appendChild(root);
+
+    var app = ReactDOM.render(<App />, root);
+
+
+    const cartButtons = document.querySelectorAll('[onecart-toggle]');
+    cartButtons && cartButtons.forEach(it => it.addEventListener('click', () => { app.showSummary() }));
+  }
+};
+
