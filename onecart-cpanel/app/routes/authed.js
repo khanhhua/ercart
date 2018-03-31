d38a2eee8d13fee8d242afa0a0d12f742353fb68
Hua Do Gia Khanh <khanh.hua@axa.com.sg>
2018-03-31 17:10:03 +0800

    Feature: - Redirect to appid subdomain (#35)

--- onecart-cpanel/app/routes/authed.js
+++ onecart-cpanel/app/routes/authed.js
@@ -1,6 +1,20 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  beforeModel(transition) {
+    const {token: uriFriendlyToken} = transition.queryParams;
+    if (uriFriendlyToken && transition.sequence === 0) {
+      const token = decodeURIComponent(uriFriendlyToken);
+      window.localStorage.setItem('onecart-token', token);
+      const [,payload,] = token.split('.');
+      const claims = JSON.parse(atob(payload));
+      this.transitionTo('authed.products', {appid: claims.sub});
+    } else if (this.get('authService.isAuthed')) {
+      return true;
+    } else {
+      this.transitionTo('login');
+    }
+  },
   model({appid}) {
     console.info(`Loading appid ${appid}...`);
     return {appid};
